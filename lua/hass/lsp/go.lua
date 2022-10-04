e0ab7bc5f3674ddc5530087f8434a393d3e05af3
Hass Khalid <me@razgriz.io>
2022-10-04 23:11:24 +0100

    reverted go lsp config back to it working

--- lua/hass/lsp/go.lua
+++ lua/hass/lsp/go.lua
@@ -14,38 +14,60 @@ neovim_lsp.gopls.setup {
 	},
 }
 
-GoImports = function(timeout_ms)
-	local context = { only = { 'source.organizeImports' } }
-	vim.validate { context = { context, 't', true } }
+function Goimports(timeoutms)
+  local context = { source = { organizeImports = true } }
+  vim.validate { context = { context, "t", true } }
 
-	local params = vim.lsp.util.make_range_params()
-	params.context = context
+  local params = vim.lsp.util.make_range_params()
+  params.context = context
 
-	-- See the implementation of the textDocument/codeAction callback
-	-- (lua/vim/lsp/handler.lua) for how to do this properly.
-	local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, timeout_ms)
-	if not result or next(result) == nil then
-		return
-	end
-	local actions = result[1].result
-	if not actions then
-		return
-	end
-	local action = actions[1]
+  local method = "textDocument/codeAction"
+  local req_s = vim.lsp.buf_request_sync(0, method, params, timeoutms)
+  if req_s and req_s[1] then
+    local result = req_s[1].result
+    if result and result[1] then
+      local edit = result[1].edit
+      vim.lsp.util.apply_workspace_edit(edit)
+    end
+  end
 
-	-- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-	-- is a CodeAction, it can have either an edit, a command or both. Edits
-	-- should be executed first.
-	if action.edit or type(action.command) == 'table' then
-		if action.edit then
-			vim.lsp.util.apply_workspace_edit(action.edit)
-		end
-		if type(action.command) == 'table' then
-			vim.lsp.buf.execute_command(action.command)
-		end
-	else
-		vim.lsp.buf.execute_command(action)
-	end
+  vim.lsp.buf.formatting_sync()
 end
 
-vim.cmd [[autocmd BufWritePre *.go lua GoImports(1000)]]
+vim.cmd [[autocmd BufWritePre *.go lua Goimports(1000)]]
+
+-- GoImports = function(timeout_ms)
+-- 	local context = { only = { 'source.organizeImports' } }
+-- 	vim.validate { context = { context, 't', true } }
+
+-- 	local params = vim.lsp.util.make_range_params()
+-- 	params.context = context
+
+-- 	-- See the implementation of the textDocument/codeAction callback
+-- 	-- (lua/vim/lsp/handler.lua) for how to do this properly.
+-- 	local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params, timeout_ms)
+-- 	if not result or next(result) == nil then
+-- 		return
+-- 	end
+-- 	local actions = result[1].result
+-- 	if not actions then
+-- 		return
+-- 	end
+-- 	local action = actions[1]
+
+-- 	-- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+-- 	-- is a CodeAction, it can have either an edit, a command or both. Edits
+-- 	-- should be executed first.
+-- 	if action.edit or type(action.command) == 'table' then
+-- 		if action.edit then
+-- 			vim.lsp.util.apply_workspace_edit(action.edit)
+-- 		end
+-- 		if type(action.command) == 'table' then
+-- 			vim.lsp.buf.execute_command(action.command)
+-- 		end
+-- 	else
+-- 		vim.lsp.buf.execute_command(action)
+-- 	end
+-- end
+
+-- vim.cmd [[autocmd BufWritePre *.go lua GoImports(1000)]]
