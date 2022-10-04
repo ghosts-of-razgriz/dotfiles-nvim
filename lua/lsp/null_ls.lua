affa95c40c74dbf6e6f5f628a44d911f86f0be25
Hass Khalid <me@razgriz.io>
2022-10-04 23:11:27 +0100

    added a check for ensuring goimports and stylua is installed

--- lua/lsp/null_ls.lua
+++ lua/lsp/null_ls.lua
@@ -1,5 +1,24 @@
 local null_ls = require 'null-ls'
 local b = null_ls.builtins
+local utils = require 'utils'
+
+if vim.fn.executable 'stylua' == 0 then
+	if vim.fn.executable 'brew' == 0 then
+		print 'Error: brew is required to install stylua'
+	end
+
+	print 'Info: Installing stylua'
+	utils.run 'brew install stylua'
+end
+
+if vim.fn.executable 'goimports' == 0 then
+	if vim.fn.executable 'go' == 0 then
+		print 'Error: go is required to install goimports'
+	end
+
+	print 'Info: Installing goimports'
+	utils.run 'go install golang.org/x/tools/cmd/goimports@latest'
+end
 
 null_ls.setup {
 	sources = {
