54e437d1f8ca288c644a3da8d51a0f7360d3b9c6
Hass Khalid <me@razgriz.io>
2022-10-04 23:11:27 +0100

    added some key mapping to dapui

--- lua/config/dap_ui.lua
+++ lua/config/dap_ui.lua
@@ -1,6 +1,14 @@
 local dapui = require 'dapui'
 local utils = require 'utils'
 
-dapui.setup {}
+dapui.setup {
+	mappings = {
+		expand = '<cr>',
+		open = 'o',
+		remove = 'd',
+		edit = 'e',
+		repl = 'r',
+	},
+}
 
 utils.set_keymap('n', '<m-d><m-d>', '<cmd>lua require("dapui").toggle()<cr>', utils.noresilent)
