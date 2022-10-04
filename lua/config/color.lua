daefb076612072beedf61615eb7f5fec7abdf933
Hass Khalid <me@razgriz.io>
2022-10-04 23:11:27 +0100

    renabled the inactive lualine transparency

--- lua/config/color.lua
+++ lua/config/color.lua
@@ -6,5 +6,6 @@ vim.g.tokyonight_italic_variables = false
 vim.g.tokyonight_lualine_bold = true
 vim.g.tokyonight_sidebars = { 'packer' }
 vim.g.tokyonight_transparent = true
+vim.g.tokyonight_hide_inactive_statusline = true
 
 vim.cmd [[colorscheme tokyonight]]
