ba0dbe9455dc5df57174b7f0f99490c10d2ea31e
Hass Khalid <me@razgriz.io>
2022-10-04 23:11:28 +0100

    added ultest config for nicer looking indicators

--- lua/config/ultest.lua
+++ lua/config/ultest.lua
@@ -1,4 +1,15 @@
-vim.g.ultest_ues_pty = 1
+vim.g.ultest_attach_width = 180
+vim.g.ultest_virtual_text = 0
+vim.g.ultest_output_cols = 120
+vim.g.ultest_max_threads = 4
+vim.g.ultest_use_pty = 1
+vim.g.ultest_pass_sign = ' '
+vim.g.ultest_fail_sign = ' '
+vim.g.ultest_running_sign = ' '
+vim.g.ultest_output_on_run = 0
+vim.g.ultest_output_on_line = 1
+
+vim.g['test#go#gotest#options'] = '-v'
 
 local wk = require 'which-key'
 
