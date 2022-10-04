52b6fd52704bfae664bb3a7790d18de20b123189
Hass Khalid <me@razgriz.io>
2022-10-04 23:11:26 +0100

    enabled more options for the tokyonight colorscheme

--- lua/config/color.lua
+++ lua/config/color.lua
@@ -3,5 +3,8 @@ vim.g.tokyonight_italic_comments = true
 vim.g.tokyonight_italic_keyworkds = true
 vim.g.tokyonight_italic_functions = false
 vim.g.tokyonight_italic_variables = false
+vim.g.tokyonight_hide_inactive_statusline = true
+vim.g.tokyonight_lualine_bold = true
+vim.g.tokyonight_sidebars = { 'packer' }
 
 vim.cmd [[colorscheme tokyonight]]
