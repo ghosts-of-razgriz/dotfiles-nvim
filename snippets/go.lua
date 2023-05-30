b83fca42f09cf707e8b0dc98befbcc728c523adf
Hass Khalid <me@razgriz.io>
2023-05-30 14:39:52 +0100

    added more go snippets

--- snippets/go.lua
+++ snippets/go.lua
@@ -5,7 +5,10 @@ local i = ls.insert_node
 local fmt = ls.extend_decorator.apply(require('luasnip.extras.fmt').fmt, { delimiters = '<>' })
 
 local snippets = {
-	s('ien', fmt('if err != nil {\n	<>\n}', { i(0) })),
+	s('if err nil', fmt('if err != nil {\n	<>\n}', { i(0) })),
+	s('if something nil', fmt('if <> != nil {\n <>\n}', { i(1), i(0) })),
+	s('gofunc', fmt('go func(<>) {\n	<>\n}()', { i(1), i(0) })),
+	s('func', fmt('func(<>) {\n	<>\n}', { i(1), i(0) })),
 }
 
 local auto_snippets = {}
