543aebaa664ebf78f19b859d325ee3c2a687bfcf
Hass Khalid <me@razgriz.io>
2023-12-26 10:51:49 +0000

    removed all plugins and plugin related config

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
 -- map leader keys
 vim.cmd [[let g:mapleader = "\<space>"]]
 vim.cmd [[let g:maplocalleader = "\<c-space>"]]
-
-require 'init-packer'
-require 'plugins'
--- lua/config/autopairs.lua
+++
@@ -1,9 +0,0 @@
-local autopairs = require 'nvim-autopairs'
-
-autopairs.setup {
-	check_ts = true,
-}
-
-require('nvim-treesitter.configs').setup {
-	autopairs = { enable = true },
-}
