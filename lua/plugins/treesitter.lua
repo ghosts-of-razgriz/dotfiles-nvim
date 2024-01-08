94fb065a327da59e247d51942bb1731b73cc50db
Hass Khalid <me@razgriz.io>
2024-01-08 08:37:35 +0000

    feat: added treesitter for syntax highlighting

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,34 @@
+return {
+		'nvim-treesitter/nvim-treesitter',
+		lazy = false,
+		config = function()
+			local t = require 'nvim-treesitter.configs'
+
+t.setup {
+	autopairs = { enable = true },
+	ensure_installed = {
+		'css',
+		'dockerfile',
+		'fish',
+		'go',
+		'gomod',
+		'html',
+		'javascript',
+		'json',
+		'lua',
+		'scss',
+		'toml',
+		'typescript',
+		'vimdoc',
+		'yaml',
+	},
+	hightlight = { enable = true },
+	indent = { enable = false },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+}
+		end,
+	}
+
