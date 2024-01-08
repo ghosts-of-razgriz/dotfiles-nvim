78b1bc25cbb8b08b475b6e4953ef03a82d3c6b5f
Hass Khalid <me@razgriz.io>
2024-01-08 15:04:00 +0000

    trial: replace autopairs with mini.pairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,9 +1,19 @@
-return {
-	'windwp/nvim-autopairs',
-	config = function()
-		local a = require 'nvim-autopairs'
+-- return {
+-- 	'windwp/nvim-autopairs',
+-- 	config = function()
+-- 		local a = require 'nvim-autopairs'
 
-		a.setup { check_ts = true }
+-- 		a.setup { check_ts = true }
+-- 	end,
+-- 	event = { 'InsertEnter' },
+-- }
+
+return {
+	'echasnovski/mini.pairs',
+	config = function()
+		local p = require 'mini.pairs'
+
+		p.setup()
 	end,
 	event = { 'InsertEnter' },
 }
