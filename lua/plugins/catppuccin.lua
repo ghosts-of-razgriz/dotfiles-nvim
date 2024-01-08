5c1d33d6e8edcd0e7d4eb5330f049e6acf18692a
Hass Khalid <me@razgriz.io>
2024-01-08 08:41:05 +0000

    feat: added catppuccin as the color scheme

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,19 @@
+return {
+		'catppuccin/nvim',
+		name = 'catppuccin',
+		lazy = false,
+		config = function()
+local c = require 'catppuccin'
+
+c.setup {
+	transparent_background = false,
+}
+
+vim.g.catppuccin_flavour = 'mocha'
+
+vim.cmd [[colorscheme catppuccin]]
+
+		end,
+		priority = 1000,
+	}
+
