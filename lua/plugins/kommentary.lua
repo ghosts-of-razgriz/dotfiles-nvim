c40c9235c7451bf70a1e20cb9c034417357c96ba
Hass Khalid <me@razgriz.io>
2024-01-08 12:05:00 +0000

    feat: added kommentary for comment operations

---
+++ lua/plugins/kommentary.lua
@@ -0,0 +1,11 @@
+return {
+	'b3nj5m1n/kommentary',
+	keys = 'gc',
+	config = function()
+		local k = require 'kommentary.config'
+
+		k.configure_language('default', {
+			prefer_single_line_comments = true,
+		})
+	end,
+}
