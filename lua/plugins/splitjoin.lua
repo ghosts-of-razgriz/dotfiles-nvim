7ebbb60e341e08b068ee57159be28f6a1afc7b2a
Hass Khalid <me@razgriz.io>
2024-01-11 13:52:52 +0000

    trial: replaced mini.splitjoin to treej

--- lua/plugins/splitjoin.lua
+++ lua/plugins/splitjoin.lua
@@ -1,13 +1,27 @@
-return {
-	'echasnovski/mini.splitjoin',
-	config = function()
-		local s = require 'mini.splitjoin'
+-- return {
+-- 	'echasnovski/mini.splitjoin',
+-- 	config = function()
+-- 		local s = require 'mini.splitjoin'
 
-		s.setup {
-			mappings = {
-				toggle = 'gs',
-			},
+-- 		s.setup {
+-- 			mappings = {
+-- 				toggle = 'gs',
+-- 			},
+-- 		}
+-- 	end,
+-- 	keys = 'gs',
+-- }
+return {
+	'Wansmer/treesj',
+	keys = { 'gs' },
+	dependencies = { 'nvim-treesitter/nvim-treesitter' },
+	config = function()
+		local t = require 'treesj'
+
+		t.setup {
+			use_default_keymaps = false,
 		}
+
+		vim.keymap.set('n', 'gs', t.toggle, { noremap = true })
 	end,
-	keys = 'gs',
 }
