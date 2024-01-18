baf29d371bfefb75bd276d6deade0a9766387531
Hass Khalid <me@razgriz.io>
2024-01-18 16:00:00 +0000

    feat: added ability to toggle auto-formatting

--- lua/lsp/conform.lua
+++ lua/lsp/conform.lua
@@ -1,10 +1,40 @@
 local c = require 'conform'
 
+vim.api.nvim_create_user_command('ConformDisable', function()
+	vim.g.conform_autoformat = false
+	print 'Conform: autoformat disabled'
+end, {
+	desc = 'Disable autoformat-on-save',
+})
+
+vim.api.nvim_create_user_command('ConformEnable', function()
+	vim.g.conform_autoformat = true
+	print 'Conform: autoformat enabled'
+end, {
+	desc = 'Enable autoformat-on-save',
+})
+
+vim.api.nvim_create_user_command('ConformToggle', function()
+	if vim.g.conform_autoformat then
+		vim.cmd 'ConformDisable'
+	else
+		vim.cmd 'ConformEnable'
+	end
+end, {
+	desc = 'Toggle autoformat-on-save',
+})
+
+vim.g.conform_autoformat = true
+
 c.setup {
-	format_on_save = {
-		lsp_fallback = false,
-		timeout_ms = 500,
-	},
+	format_on_save = function()
+		if vim.g.conform_autoformat then
+			return {
+				lsp_fallback = false,
+				timeout_ms = 500,
+			}
+		end
+	end,
 	notify_on_error = false,
 }
 
@@ -27,3 +57,5 @@ if vim.fn.executable 'prettierd' == 1 then
 	c.formatters_by_ft.json = { 'prettierd' }
 	c.formatters_by_ft.css = { 'prettierd' }
 end
+
+vim.keymap.set('n', '<space><space>f', '<cmd>ConformToggle<cr>', { noremap = true })
