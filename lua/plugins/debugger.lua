c9c5b6821ac528d8b933a5326b35e63dc1d2f154
Hass Khalid <me@razgriz.io>
2024-02-17 06:44:59 +0000

    task: configured dap-ui

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -10,14 +10,50 @@ local install_debugpy = function()
 		:wait()
 end
 
-local dap_init = function() end
+local dap_init = function()
+	local dapui = require('dapui')
+	local leader = '<leader>'
+	local nore = { noremap = true }
+
+	vim.keymap.set('n', leader .. 'db', '<cmd>DapToggleBreakpoint<cr>', nore) -- Telescope <prompt>
+	vim.keymap.set('n', leader .. 'dd', function()
+		dapui.toggle()
+	end, nore) -- Telescope <prompt>
+end
+
+local dap_config = function()
+	local dap = require('dap')
+	local dapui = require('dapui')
+
+	dapui.setup()
+	dap.listeners.after.event_initialized['dapui_config'] = function()
+		dapui.open()
+	end
+	dap.listeners.after.event_terminated['dapui_config'] = function()
+		dapui.close()
+	end
+	dap.listeners.after.event_exited['dapui_config'] = function()
+		dapui.close()
+	end
+end
 
 return {
 	'rcarriga/nvim-dap-ui',
 	event = 'VeryLazy',
 	init = dap_init,
+	config = dap_config,
 	dependencies = {
 		'mfussenegger/nvim-dap',
+		{
+			'folke/neodev.nvim',
+			config = function()
+				local n = require('neodev')
+
+				n.setup({
+					library = { plugins = { 'nvim-dap-ui' }, types = true },
+				})
+			end,
+		},
 		{
 			'mfussenegger/nvim-dap-python',
 			build = install_debugpy,
