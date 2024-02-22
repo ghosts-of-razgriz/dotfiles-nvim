7038ae082ff79693bd894415ab53fc9243fa6211
Hass Khalid <me@razgriz.io>
2024-02-22 04:22:01 +0000

    fix: added the build command to update grammar on plugin updates

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,43 +1,24 @@
 return {
 	'nvim-treesitter/nvim-treesitter',
+	build = ':TSUpdate',
 	config = function()
 		local t = require('nvim-treesitter.configs')
 		local tc = require('treesitter-context')
 
 		t.setup({
 			autopairs = { enable = true },
-			ensure_installed = {
-				'bash',
-				'bibtex',
-				'css',
-				'dockerfile',
-				'fish',
-				'git_config',
-				'git_rebase',
-				'go',
-				'gomod',
-				'gosum',
-				'html',
-				'javascript',
-				'json',
-				'kdl',
-				'lua',
-				'make',
-				'markdown',
-				'markdown_inline',
-				'python',
-				'regex',
-				'scss',
-				'sql',
-				'terraform',
-				'toml',
-				'typescript',
-				'vim',
-				'vimdoc',
-				'yaml',
-			},
+			auto_install = true,
 			hightlight = { enable = true },
 			indent = { enable = false },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = '<M-t><M-t>',
+					node_incremental = '<M-t><M-t>',
+					scope_incremental = '<M-t><M-s>',
+					node_decremental = '<M-t><M-d>',
+				},
+			},
 			context_commentstring = {
 				enable = true,
 				enable_autocmd = false,
@@ -45,9 +26,11 @@ return {
 		})
 
 		tc.setup()
+
+		vim.o.foldmethod = 'expr'
+		vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 	end,
 	dependencies = {
-		-- 'JoosepAlviste/nvim-ts-context-commentstring',
 		'nvim-treesitter/nvim-treesitter-context',
 	},
 }
