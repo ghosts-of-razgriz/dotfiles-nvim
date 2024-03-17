bbf4c765992c4964d547f88861645bb45f148632
Hass Khalid <me@razgriz.io>
2024-03-17 08:43:57 +0000

    fix: #1 switched to Neogit from fugitive

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,19 @@
+return {
+	'NeogitOrg/neogit',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		'sindrets/diffview.nvim',
+		'nvim-telescope/telescope.nvim',
+		'ibhagwan/fzf-lua',
+	},
+	cmd = { 'Neogit' },
+	config = function()
+		require('neogit').setup({})
+	end,
+	init = function()
+		local leader = '<leader>'
+
+		local opts = { noremap = true }
+		vim.keymap.set('n', leader .. 'gs', '<cmd>Neogit<cr>', opts)
+	end,
+}
