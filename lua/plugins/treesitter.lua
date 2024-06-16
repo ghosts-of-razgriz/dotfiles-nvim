3f27c07f9f785e39e95ef0ad8a0d98de1ad6fed7
Hass Khalid <me@razgriz.io>
2024-06-16 02:09:19 +0100

    imprv: enabled treesitter to auto start on FileType

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -17,7 +17,19 @@ return {
 
 		require('nvim-treesitter.install').prefer_git = true
 	end,
-	dependencies = {
-		{ 'nvim-treesitter/nvim-treesitter-context', opt = {} },
-	},
+	init = function()
+		local ts = require('nvim-treesitter.config')
+
+		vim.api.nvim_create_autocmd('FileType', {
+			callback = function(args)
+				local buf = args.buf
+				local ft = vim.bo[buf].filetype
+				local lang = vim.treesitter.language.get_lang(ft) or ft
+
+				if vim.list_contains(ts.installed_parsers(), lang) then
+					pcall(vim.treesitter.start, buf, lang)
+				end
+			end,
+		})
+	end,
 }
