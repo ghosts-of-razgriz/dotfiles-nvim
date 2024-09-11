65acb75a34937a130f117a97e9a148505aabefd2
Hass Khalid <me@razgriz.io>
2024-09-11 07:49:42 +0100

    imprv: cmp emmet

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,7 +6,7 @@ return {
 		'hrsh7th/cmp-path',
 		'hrsh7th/cmp-nvim-lua',
 		'hrsh7th/cmp-nvim-lsp',
-		{ 'jackieaskins/cmp-emmet', build = 'npm run release'  },
+		{ 'jackieaskins/cmp-emmet', build = 'npm run release', dir = '~/code/cmp-emmet' },
 		'onsails/lspkind-nvim',
 		{
 			'L3MON4D3/LuaSnip',
@@ -102,7 +102,7 @@ return {
 						nvim_lua = '[api]',
 						buffer = '[buf]',
 						path = '[path]',
-						emmet = '[em]'
+						emmet = '[em]',
 					},
 				}),
 			},
