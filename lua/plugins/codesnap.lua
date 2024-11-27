c00042a3fb0f862574f4d97f68ad58a592c2a703
Hass Khalid <me@razgriz.io>
2024-11-27 11:52:40 +0000

    indentation on codesnap

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -1,17 +1,18 @@
 return {
-  "mistricky/codesnap.nvim",
-  build = "make build_generator",
-  keys = {
-    { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
-    { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
-  },
-  opts = {
-	mac_window_bar = false,
-    save_path = "~/Desktop",
-    has_breadcrumbs = false,
-	bg_padding = 0,
-    bg_theme = "bamboo",
-	code_font_family = "Nerd Operator",
-	watermark = "",
-  },
+	'mistricky/codesnap.nvim',
+	build = 'make build_generator',
+	keys = {
+		{ '<leader>cc', '<cmd>CodeSnap<cr>', mode = 'x' },
+		{ '<leader>cs', '<cmd>CodeSnapSave<cr>', mode = 'x' },
+	},
+	cmd = { 'CodeSnap', 'CodeSnapSave' },
+	opts = {
+		mac_window_bar = false,
+		save_path = '~/Desktop',
+		has_breadcrumbs = false,
+		bg_padding = 0,
+		bg_theme = 'bamboo',
+		code_font_family = 'Nerd Operator',
+		watermark = '',
+	},
 }
