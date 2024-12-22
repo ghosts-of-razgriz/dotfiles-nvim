012e2fea3002829dd5c4bec31ef3b0e2784a806f
Hass Khalid <me@razgriz.io>
2024-12-22 12:59:35 +0000

    disabled plugins for docker build

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -1,5 +1,13 @@
 return {
 	'mistricky/codesnap.nvim',
+	enabled = function()
+		local neovim_build = os.getenv('NEOVIM_BUILD')
+		if neovim_build then
+			return false
+		else
+			return true
+		end
+	end,
 	build = 'make build_generator',
 	keys = {
 		{ '<leader>cc', '<cmd>CodeSnap<cr>', mode = 'x' },
--- lua/plugins/ft-markdown-peek.lua
+++ lua/plugins/ft-markdown-peek.lua
@@ -1,5 +1,13 @@
 return {
 	'toppair/peek.nvim',
+	enabled = function()
+		local neovim_build = os.getenv('NEOVIM_BUILD')
+		if neovim_build then
+			return false
+		else
+			return true
+		end
+	end,
 	cmd = { 'PeekOpen', 'PeekClose' },
 	build = 'deno task --quiet build:fast',
 	config = function()
