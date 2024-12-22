df814198436ab680e28cb5c07cf26ad6793243d9
Hass Khalid <me@razgriz.io>
2024-12-22 12:44:33 +0000

    removed the local version of emmet

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,7 +6,7 @@ return {
 		'hrsh7th/cmp-path',
 		'hrsh7th/cmp-nvim-lua',
 		'hrsh7th/cmp-nvim-lsp',
-		{ 'jackieaskins/cmp-emmet', build = 'npm run release', dir = '~/code/cmp-emmet' },
+		{ 'jackieaskins/cmp-emmet', build = 'npm run release' },
 		'onsails/lspkind-nvim',
 		{
 			'L3MON4D3/LuaSnip',
