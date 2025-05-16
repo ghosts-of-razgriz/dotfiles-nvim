7c7ded2977c2be7c887f15a93429fe3ee3b5346c
Hass Khalid <me@razgriz.io>
2025-05-16 01:47:28 +0100

    added blink-cmp

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -3,8 +3,22 @@ return {
 		'rebelot/kanagawa.nvim',
 		priority = 1001,
 		opts = { theme = 'lotus' },
+	},
+	{
+		'catppuccin/nvim',
+		name = 'catppuccin',
+		priority = 1003,
+			opts = {
+				flavour = 'macchiato',
+				integrations = {
+					notify = true,
+				},
+				styles = {
+					keywords = { 'italic' },
+				},
+			},
 		config = function()
-			vim.cmd.colorscheme([[kanagawa]])
+			vim.cmd.colorscheme([[catppuccin]])
 		end,
-	}
+	},
 }
