8c90239e7c8fe92f03fbea54a742fcd42060a1cb
Hass Khalid <me@razgriz.io>
2025-05-16 14:56:22 +0100

    system clipboard copier

---
+++ lua/plugins/copier.lua
@@ -0,0 +1,17 @@
+return {
+	'haphamdev/copier.nvim',
+	dependencies = {
+		'zdcthomas/yop.nvim',
+	},
+	opts = {},
+	keys = {
+		{ 'cp', mode = 'n' },
+		{ 'cP', mode = 'n' },
+		{ 'cv', mode = 'n' },
+		{ 'cV', mode = 'n' },
+		{ 'cp', mode = 'v' },
+		{ 'cP', mode = 'v' },
+		{ 'cv', mode = 'v' },
+		{ 'cV', mode = 'v' },
+	},
+}
