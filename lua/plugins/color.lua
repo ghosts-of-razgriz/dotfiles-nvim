22debc3fb628ba178e28a60c7ad60cb80d6884c6
Hass Khalid <me@razgriz.io>
2025-08-08 23:23:39 +0100

    added oil, telescope, neogit and catppuccin

---
+++ lua/plugins/color.lua
@@ -0,0 +1,17 @@
+return {
+	'catppuccin/nvim',
+	name = 'catppuccin',
+	priority = 1003,
+	opts = {
+		flavour = 'macchiato',
+		integrations = {
+			notify = true
+		},
+		styles = {
+			keywords = { 'italic'}
+		}
+	},
+	config = function()
+	vim.cmd.colorscheme([[catppuccin]])
+	end
+}
