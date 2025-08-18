eeb71a75f7cac9a452a0b0ba4d95313d190d9a1e
Hass Khalid <me@razgriz.io>
2025-08-18 13:41:21 +0100

    switched colors for fun

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,18 +1,42 @@
 return {
-	'catppuccin/nvim',
-	name = 'catppuccin',
-	priority = 1003,
-	opts = {
-		flavour = 'macchiato',
-		integrations = {
-			notify = true,
-			blink_cmp = { style = 'bordered' },
+	{
+		'catppuccin/nvim',
+		name = 'catppuccin',
+		priority = 1003,
+		opts = {
+			flavour = 'mocha',
+			integrations = {
+				notify = true,
+				blink_cmp = { style = 'bordered' },
+			},
+			styles = {
+				keywords = { 'italic' },
+			},
 		},
-		styles = {
-			keywords = { 'italic' },
+		config = function()
+			vim.cmd.colorscheme([[catppuccin]])
+		end,
+	},
+	{
+		'folke/tokyonight.nvim',
+		priority = 1000,
+		-- init = function()
+		-- 	vim.cmd.colorscheme([[tokyonight]])
+		-- end,
+		opts = {
+			style = 'moon',
 		},
 	},
-	config = function()
-		vim.cmd.colorscheme([[catppuccin]])
-	end,
+	{
+		'scottmckendry/cyberdream.nvim',
+		priority = 1000,
+		-- init = function()
+		-- 	vim.cmd.colorscheme([[cyberdream]])
+		-- end,
+		opts = {
+			variant = 'dark',
+			italic_comments = true,
+			transparent = false,
+		},
+	},
 }
