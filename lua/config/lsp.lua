49f8651b9ff2fa6ff21e0b20288481b551ab0642
Hass Khalid <me@razgriz.io>
2025-08-28 11:08:31 +0100

    created some lsp control commmands

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -51,6 +51,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 		map(prefix .. prefix .. 'q', '<cmd>LspRestart<cr>')
 		map(prefix .. prefix .. 'i', '<cmd>LspInfo<cr>')
 		map(prefix .. prefix .. 'c', '<cmd>ConformInfo<cr>')
+		map(prefix .. prefix .. 'h', '<cmd>InlayHintToggle<cr>')
 
 		map('<c-n>', function()
 			vim.diagnostic.jump({ count = 1 })
@@ -76,4 +77,70 @@ vim.diagnostic.config({
 			[vim.diagnostic.severity.INFO] = '',
 		},
 	},
+})
+
+local inlay_hint = vim.lsp.inlay_hint
+
+vim.api.nvim_create_user_command('InlayHintDisable', function()
+	if inlay_hint.is_enabled() then
+		inlay_hint.enable(false, nil)
+		print('Lsp: inlay hints disabled')
+	end
+end, { desc = 'Disable inlay hints' })
+
+vim.api.nvim_create_user_command('InlayHintEnable', function()
+	if not inlay_hint.is_enabled() then
+		inlay_hint.enable(true, nil)
+		print('Lsp: inlay hints enabled')
+	end
+end, { desc = 'Enable inlay hints' })
+
+vim.api.nvim_create_user_command('InlayHintToggle', function()
+	if inlay_hint.is_enabled() then
+		inlay_hint.enable(false, nil)
+		print('Lsp: inlay hints disabled')
+	else
+		inlay_hint.enable(true, nil)
+		print('Lsp: inlay hints enabled')
+	end
+end, { desc = 'Toggle inlay hints' })
+
+vim.api.nvim_create_user_command(
+	'LspInfo',
+	':checkhealth vim.lsp',
+	{ desc = 'Alias to `:checkhealth vim.lsp`' }
+)
+
+vim.api.nvim_create_user_command('LspRestart', function(info)
+	local clients = info.fargs
+
+	-- Default to restarting all active servers
+	if #clients == 0 then
+		clients = vim.iter(vim.lsp.get_clients())
+			:map(function(client)
+				return client.name
+			end)
+			:totable()
+	end
+
+	for _, name in ipairs(clients) do
+		if vim.lsp.config[name] == nil then
+			vim.notify(("Invalid server name '%s'"):format(name))
+		else
+			vim.lsp.enable(name, false)
+		end
+	end
+
+	local timer = assert(vim.uv.new_timer())
+	timer:start(500, 0, function()
+		for _, name in ipairs(clients) do
+			vim.schedule_wrap(function(x)
+				vim.lsp.enable(x)
+			end)(name)
+		end
+	end)
+end, {
+	desc = 'Restart the given client',
+	nargs = '?',
+	complete = complete_client,
 })
